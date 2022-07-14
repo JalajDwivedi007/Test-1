6bf40d4b5b0f575d25c1580a38e380ae6f2f78c5
JalajDwivedi007 <109264693+JalajDwivedi007@users.noreply.github.com>
2022-07-14 15:10:19 +0530

    New commit

--- MiniProject/src/ComplaintMain.java
+++ MiniProject/src/ComplaintMain.java
@@ -58,6 +58,10 @@ public static void main(String[] args) {
 		
 		}
 		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
 	}
 	
 }
